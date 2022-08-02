a7d169f8dd6e5a1e694552440f364ed1f6b79b32
Farid0558475484 <109238744+Farid0558475484@users.noreply.github.com>
2022-08-02 19:01:05 +0400

    3.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Application
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int num1 = 10;
+            int num2 = 1000;
+            for (int i = num1; i < num2; i++)
+
+            {
+                if (i % 7 == 0) ;
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
